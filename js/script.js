d1bdccff95e2c06ecb5ef475f1bbb1e1a12f7c41
Jeff Berman <jeffuberman@gmail.com>
2015-12-16 11:28:16 -0600

    add function that can get poll with both produce types, with testing

--- js/script.js
+++ js/script.js
@@ -10,6 +10,23 @@ var extremeProduce = (function(){
       catch(error) {
         console.log("Unable to get recent poll by type: " + error);
       }
+    },
+
+    //get poll containing both veggies and fruits
+    _getRecentPoll : function(cb){
+      try {
+        var dfd = $.Deferred();
+        var dVeg = this._getPollByType("veggies");
+        var dFru = this._getPollByType("fruits");
+        $.when(dVeg, dFru)
+        .done(function(dataVeg, dataFru){
+          dfd.resolve(cb(dataVeg.concat(dataFru)));
+        });
+        return dfd.promise();        
+      }
+      catch (error) {
+        console.log("Unable to get recent poll: " + error);
+      }
     }
   }  
 }(jQuery));  
--- spec/tests.js
+++ spec/tests.js
@@ -1,11 +1,23 @@
 var expect = chai.expect;
 
 describe("Hardcore Produce Unit Tests", function(){
+  it("can create a new instance of extremeProduce", function(done){
+    expect(extremeProduce).to.have.property("_getPollByType");
+    done();
+  });
 
   it("can get a poll by produce type", function(done){
     $.when(extremeProduce._getPollByType("veggies"))
       .then(function(data){
         expect(data[0].name).to.equal("Adzuki Beans");
+        done();
+      });
+  });
+
+  it("can get a poll with both produce types", function(done){
+    $.when(extremeProduce._getRecentPoll(function(data){return data}))
+      .done(function(data){
+        expect(data).to.have.length(20);
         done();
       });
   });
